e5de3d4057a1e836e1000ca3cdddaf07b890ac7e
“akiverin” <kiverin03@yandex.ru>
2023-04-12 23:11:21 +0300

    create menu and submenu components, settings routers

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,31 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../pages/HomePage.vue';
+import AboutPage from '../pages/AboutPage.vue';
+import EntrancePage from '../pages/EntrancePage.vue';
+import NewsPage from '../pages/NewsPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: '/',
+      name: 'home',
+      component: HomePage,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: '/about',
+      name: 'about',
+      component: AboutPage,
+    },
+    {
+      path: '/entrance',
+      name: 'entrance',
+      component: EntrancePage,
+    },
+    {
+      path: '/news',
+      name: 'news',
+      component: NewsPage,
     },
   ],
 });
