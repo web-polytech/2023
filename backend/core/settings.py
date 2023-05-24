43d0f00fbda37c61f3bb3ec9c714526629494a8c
wtf403 <wtf403@yandex.ru>
2023-05-24 11:49:26 +0300

    fix nignx

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "on"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "our-school.space",
+    "localhost",
+]
 
 
 # Application definition
