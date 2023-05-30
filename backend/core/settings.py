bd1d0d5cc48b5ba9264758dd863d56a510cabf8a
wtf403 <wtf403@yandex.ru>
2023-05-30 23:30:48 +0300

    fix cors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,11 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "on"
 
-ALLOWED_HOSTS = [
-    "our-school.space",
-    "localhost",
-    "127.0.0.1"
-]
+ALLOWED_HOSTS = ["our-school.space", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -60,7 +56,7 @@ INSTALLED_APPS = [
     "celery",
     "django_celery_beat",
     "redis",
-    'flower',
+    "flower",
 ]
 
 MIDDLEWARE = [
@@ -74,9 +70,14 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     "https://our-school.space",
+    "https://localhost:8080",
+    "https://localhost:5173",
 ]
+
+CSRF_TRUSTED_ORIGINS = ["https://our-school.space", "https://localhost:8080"]
 
 ROOT_URLCONF = "core.urls"
 
@@ -167,28 +168,28 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {"ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'savenckoa@gmail.com'
+EMAIL_HOST_USER = "savenckoa@gmail.com"
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = 'savenckoa@gmail.com'
+DEFAULT_FROM_EMAIL = "savenckoa@gmail.com"
 
 # Broker settings
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
 # Celery configuration
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = TIME_ZONE
 
 # django-celery-beat configuration
 CELERY_BEAT_SCHEDULE = {
-    'send_email_every_week': {
-        'task': 'app.tasks.send_admission_emails',
-        'schedule': crontab(day_of_week='monday', hour=9),
+    "send_email_every_week": {
+        "task": "app.tasks.send_admission_emails",
+        "schedule": crontab(day_of_week="monday", hour=9),
     },
 }
