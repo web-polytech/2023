e80dcbdb2b9fb718d095a544b6473dcea9dafba4
wtf403 <wtf403@yandex.ru>
2023-06-05 03:15:33 +0300

    update urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,9 +19,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-    # Swagger documentation URLs
+    path("api/admin/", admin.site.urls),
     path(
         "api/swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
