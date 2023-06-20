338d83462185580dea1af68a52244ac54902b73b
unknown <ilya-bondar-2003@mail.ru>
2023-06-20 20:58:13 +0300

    Setup

--- backend/authentication/managers.py
+++ backend/authentication/managers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, name, specialization, password=None):
+    def create_user(self, email, name , specialization, password=None):
         user = self.model(
             email=self.normalize_email(email),
             name=name,
